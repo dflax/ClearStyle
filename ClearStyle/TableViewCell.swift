f6e53da401d4a96deec977d9903bd92cf90faa62
Daniel Flax <dflax@yahoo.com>
2015-04-14 11:22:58 -0400

    Swipe to delete working.

--- ClearStyle/TableViewCell.swift
+++ ClearStyle/TableViewCell.swift
@@ -9,9 +9,26 @@
 import UIKit
 import QuartzCore
 
+// A protocol that the TableViewCell uses to inform its delegate of state change
+protocol TableViewCellDelegate {
+	// indicates that the given item has been deleted
+	func toDoItemDeleted(todoItem: ToDoItem)
+}
+
 class TableViewCell: UITableViewCell {
 
+	// Gradient layer properties
 	let gradientLayer = CAGradientLayer()
+
+	// Gesture recognizer properties
+	var originalCenter = CGPoint()
+	var deleteOnDragRelease = false
+
+	// will aid with the delegate protocol
+	// The object that acts as delegate for this cell.
+	var delegate: TableViewCellDelegate?
+	// The item that this cell renders.
+	var toDoItem: ToDoItem?
 
 	required init(coder aDecoder: NSCoder) {
 		fatalError("NSCoding not supported")
@@ -30,11 +47,61 @@ class TableViewCell: UITableViewCell {
 		gradientLayer.colors = [color1, color2, color3, color4]
 		gradientLayer.locations = [0.0, 0.01, 0.95, 1.0]
 		layer.insertSublayer(gradientLayer, atIndex: 0)
+
+		// add a pan recognizer
+		var recognizer = UIPanGestureRecognizer(target: self, action: "handlePan:")
+		recognizer.delegate = self
+		addGestureRecognizer(recognizer)
 	}
 
 	override func layoutSubviews() {
 		super.layoutSubviews()
 		gradientLayer.frame = bounds
+	}
+
+	// MARK: - horizontal pan gesture methods
+	func handlePan(recognizer: UIPanGestureRecognizer) {
+
+		// 1
+		if recognizer.state == .Began {
+			// when the gesture begins, record the current center location
+			originalCenter = center
+		}
+
+		// 2
+		if recognizer.state == .Changed {
+			let translation = recognizer.translationInView(self)
+			center = CGPointMake(originalCenter.x + translation.x, originalCenter.y)
+			// has the user dragged the item far enough to initiate a delete/complete?
+			deleteOnDragRelease = frame.origin.x < -frame.size.width / 2.0
+		}
+
+		// 3
+		if recognizer.state == .Ended {
+			// the frame this cell had before user dragged it
+			let originalFrame = CGRect(x: 0, y: frame.origin.y, width: bounds.size.width, height: bounds.size.height)
+			if !deleteOnDragRelease {
+				// if the item is not being deleted, snap back to the original location
+				UIView.animateWithDuration(0.2, animations: {self.frame = originalFrame})
+			}
+			if deleteOnDragRelease {
+				if delegate != nil && toDoItem != nil {
+					// notify the delegate that this item should be deleted
+					delegate!.toDoItemDeleted(toDoItem!)
+				}
+			}
+		}
+	}
+
+	override func gestureRecognizerShouldBegin(gestureRecognizer: UIGestureRecognizer) -> Bool {
+		if let panGestureRecognizer = gestureRecognizer as? UIPanGestureRecognizer {
+			let translation = panGestureRecognizer.translationInView(superview!)
+			if fabs(translation.x) > fabs(translation.y) {
+				return true
+			}
+			return false
+		}
+		return false
 	}
 
 }
--- ClearStyle/ViewController.swift
+++ ClearStyle/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, TableViewCellDelegate {
 
 	@IBOutlet weak var tableView: UITableView!
 	var toDoItems = [ToDoItem]()
@@ -19,9 +19,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 		// Set up the TableView stuff
 		tableView.dataSource = self
 		tableView.delegate = self
-//		tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
 		tableView.registerClass(TableViewCell.self, forCellReuseIdentifier: "cell")
 		tableView.separatorStyle = .None
+		tableView.backgroundColor = UIColor.blackColor()
 		tableView.rowHeight = 50.0
 
 		if toDoItems.count > 0 {
@@ -52,15 +52,37 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
  
 	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as TableViewCell
+		cell.selectionStyle = .None
 		cell.textLabel?.backgroundColor = UIColor.clearColor()
 
 		let item = toDoItems[indexPath.row]
 		cell.textLabel?.text = item.text
+
+		// For the TableViewCell delegate protocol
+		cell.delegate = self
+		cell.toDoItem = item
+
 		return cell
 	}
+
 	// Necessary for older versions of iOS
 	func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
 		return tableView.rowHeight;
+	}
+
+	// TableViewCell DelegateProtocol
+	func toDoItemDeleted(toDoItem: ToDoItem) {
+		let index = (toDoItems as NSArray).indexOfObject(toDoItem)
+		if index == NSNotFound { return }
+
+		// could removeAtIndex in the loop but keep it here for when indexOfObject works
+		toDoItems.removeAtIndex(index)
+
+		// use the UITableView to animate the removal of this row
+		tableView.beginUpdates()
+		let indexPathForRow = NSIndexPath(forRow: index, inSection: 0)
+		tableView.deleteRowsAtIndexPaths([indexPathForRow], withRowAnimation: .Fade)
+		tableView.endUpdates()
 	}
 
 	// MARK: - UITableView delegate methods
