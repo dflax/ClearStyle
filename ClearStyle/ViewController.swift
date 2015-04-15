4b300b7fc8cd4eb4585a3c619ae2fb1d239cf351
Daniel Flax <dflax@yahoo.com>
2015-04-15 16:21:55 -0400

    Pull to add completed.

--- ClearStyle/ViewController.swift
+++ ClearStyle/ViewController.swift
@@ -71,6 +71,24 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 	}
 
 	// MARK: - TableViewCellDelegate methods
+
+	func toDoItemAdded() {
+		let toDoItem = ToDoItem(text: "")
+		toDoItems.insert(toDoItem, atIndex: 0)
+		tableView.reloadData()
+
+		// enter edit mode
+		var editCell: TableViewCell
+		let visibleCells = tableView.visibleCells() as [TableViewCell]
+		for cell in visibleCells {
+			if (cell.toDoItem === toDoItem) {
+				editCell = cell
+				editCell.label.becomeFirstResponder()
+				break
+			}
+		}
+	}
+
 	func cellDidBeginEditing(editingCell: TableViewCell) {
 		var editingOffset = tableView.contentOffset.y - editingCell.frame.origin.y as CGFloat
 		let visibleCells = tableView.visibleCells() as [TableViewCell]
@@ -152,6 +170,31 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 			// add the placeholder
 			tableView.insertSubview(placeHolderCell, atIndex: 0)
 		}
+	}
+
+	func scrollViewDidScroll(scrollView: UIScrollView!) {
+		var scrollViewContentOffsetY = scrollView.contentOffset.y
+
+		if pullDownInProgress && scrollView.contentOffset.y <= 0.0 {
+			// maintain the location of the placeholder
+			placeHolderCell.frame = CGRect(x: 0, y: -tableView.rowHeight, width: tableView.frame.size.width, height: tableView.rowHeight)
+			placeHolderCell.label.text = -scrollViewContentOffsetY > tableView.rowHeight ? "Release to add item" : "Pull to add item"
+			placeHolderCell.alpha = min(1.0, -scrollViewContentOffsetY / tableView.rowHeight)
+		} else {
+			pullDownInProgress = false
+		}
+	}
+
+	func scrollViewDidEndDragging(scrollView: UIScrollView!, willDecelerate decelerate: Bool) {
+		// check whether the user pulled down far enough
+		if pullDownInProgress && -scrollView.contentOffset.y > tableView.rowHeight {
+			if pullDownInProgress && -scrollView.contentOffset.y > tableView.rowHeight {
+				toDoItemAdded()
+			}
+		}
+
+		pullDownInProgress = false
+		placeHolderCell.removeFromSuperview()
 	}
 
 	// MARK: - UITableView delegate methods
